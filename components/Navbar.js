eb67bd527e130581ce4baea5992b0ce0c5b950c1
Piotr <piotr.bania@europe.com>
2023-01-01 23:50:36 +0000

    init

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,15 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import logo from '../public/logo/tsunami_logo.svg'
+import burger from '../public/logo/burger.svg'
 
 const Navbar = () => {
     return (
         <section className='navbar'>
             <div className='header'>
-                <div className="burger">|||</div>
+                <div className="burger">
+                    <Image className='burger' src={burger} alt="menu" width={50} height={28} />
+                </div>
                 <Link href="/">
                     <Image className='logo' src={logo} alt="logo" width={120} height={28} />
                 </Link>
